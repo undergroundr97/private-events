36c6f26e38238eacb1262ba2eb3e0c669fa68efe
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-08-18 16:01:32 -0300

    helper for image.privacy

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,6 @@
 module EventsHelper
+  def lock_image_for(privacystatus)
+    privacy_icons = { false => 'not-locked.svg', true => 'locked.svg' }
+    image_tag(privacy_icons[privacystatus], class: 'privacy-icon')
+  end
 end
